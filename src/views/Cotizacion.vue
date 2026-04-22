86ff691caa3e92cb5e52ff8074991cdffb649b97
Luis Lopez <desarrollo3@priego8a.com>
2026-04-22 13:20:24 -0600

    cambios en Promocion

--- src/views/Cotizacion.vue
+++ src/views/Cotizacion.vue
@@ -2578,11 +2578,20 @@ const registrarCerrarConEsc = (mostrarVista) => {
 /*************************************/
 /*      AL MONTAR COMPONENTE
     /*************************************/
-onMounted(() => {
-  const el = modalRef.value;
-  el?.addEventListener("hidden.bs.modal", resetTabla);
-});
 
+    onMounted(() => {
+  const el = modalRef.value;
+
+  el?.addEventListener("hidden.bs.modal", () => {
+    resetTabla();
+    cargarPromosRapidas();
+  });
+
+  // opcional: cuando se abre
+  el?.addEventListener("shown.bs.modal", () => {
+    cargarPromosRapidas();
+  });
+});
 let cleanupEscListener = null; // <-- DECLARADO ANTES DE onMounted
 
 onMounted(async () => {
@@ -2845,6 +2854,7 @@ const agregarLlanta = async (item) => {
 
   //console.log('agregarLlanta2: '+ JSON.stringify(cotizacionForm.llantas))
 };
+
 
 // Obtener promociones aplicables a una llanta (por inventario)
 const obtenerPromosPorInventario = async (idInventarioInicial) => {
